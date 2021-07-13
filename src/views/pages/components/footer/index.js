cbaad7fbead98441788eb0ac97bad1bc4a7f92e4
geninha <geninhaa12@gmail.com>
2021-07-13 15:32:36 -0300

    agora foi tudo

---
+++ src/views/pages/components/footer/index.js
@@ -0,0 +1,18 @@
+import Planet from '../../../../assets/images/elements/planet.png';
+
+let footer = `
+<div class="footer-contain">
+    <div class="footer-left--text">
+        <p>
+            Star wars  -  este é um exemplo criado para os Alunos Gama Academy</br>
+            Módulo de boas práticas.
+            </p>
+            <a href="#section-one">
+                <img src="${Planet}" alt="planet">
+            </a>
+    </div>
+</div>
+
+`
+
+export default footer;
