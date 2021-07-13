f54318a8ee727d27108f114402279b26e4e0ac5d
geninha <geninhaa12@gmail.com>
2021-07-13 15:31:32 -0300

    quase la

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,10 +1,16 @@
+import HomeSectionOne from '../components/sections/homeSectionOne';
+import HomeSectionTwo from '../components/sections/homeSectionTwo';
+import floatImage from '../components/float/floatimage';
+
 let Home = {
   is_private: false,
 
   render: async () => {
       let view = `
           <div>
-            <h1>Home</div>
+            ${HomeSectionOne}
+            ${HomeSectionTwo}
+            ${floatImage}
           </div>
       `;
 
